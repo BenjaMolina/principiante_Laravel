dae4858cc971f267d67c19e5fc6c629d2d24266a
BenjaMolina <molinasb.94@gmail.com>
2018-04-23 17:04:49 -0500

    Rutas - Tipos de rutas

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,41 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'Hola desde la pagina de inicio';
 });
+
+Route::get('contacto', function () {
+    return 'Hola desde la pagina de contaco';
+});
+
+//Rutas con pase de parametro necesario
+Route::get('saludos/{nombre}', function ($nombre) {
+    return "HOLA $nombre";
+});
+
+//Rutas con pase de parametro Opcionales
+Route::get('saludos2/{nombre?}', function ($nombre = "Por defecto") {
+    return "Hola $nombre";
+});
+
+//Rutas con pase de parametro Validados (En este caso no acepta numero)
+Route::get('saludos3/{nombre?}', function ($nombre = "Por defecto") {
+    return "Hola $nombre";
+})->where('nombre', "[A-Za-z]+");
+
+
+//Rutas con Alias
+Route::get('rutas', function () {
+    echo "<a href=" . route('contactos') . "> Contactanos </a><br>";
+    echo "<a href=" . route('contactos') . "> Contactanos </a><br>";
+    echo "<a href=" . route('contactos') . "> Contactanos </a><br>";
+    echo "<a href=" . route('contactos') . "> Contactanos </a><br>";
+});
+
+// Route::get('contacto',['as' => 'contactos', function(){
+//     return "Seccion de contactos";
+// }]);
+
+Route::get('contactooo', ['as' => 'contactos', function () {
+    return "Seccion de contactos";
+}]);
