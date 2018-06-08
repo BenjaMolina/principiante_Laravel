8197b49cacc463fe9163388f1bb9da7117a94780
BenjaMolina <molinasb.94@gmail.com>
2018-06-08 00:22:49 -0500

    019 Preprocesador, bootstrap y Webpack

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,11 +2,11 @@
 
 @section('content')
     <h1>LOGIN</h1>
-    <form method="POST" action"{{route('login')}}">
+    <form class="form-inline" method="POST" action"{{route('login')}}">
         {!! csrf_field() !!} <!--se usa para el token-->
-        <input type="email" name="email" placeholder="Email">
-        <input type="password" name="password" placeholder="password">
-        <input type="submit" value="Entrar">
+        <input class="form-control" type="email" name="email" placeholder="Email">
+        <input class="form-control" type="password" name="password" placeholder="password">
+        <input class="btn btn-primary" type="submit" value="Entrar">
     </form>
     <br>
 @stop
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -6,16 +6,8 @@
     <title>Mi sitio</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <style>
-        .active{
-            text-decoration: none;
-            color:green;
-        }
-        .error{
-            color:red;
-            font-size: 12px;
-        }
-    </style>
+    <link rel="stylesheet" href="/css/app.css">
+    
 </head>
 <body>
     <header>
@@ -28,42 +20,92 @@
                 return request()->is($url) ? 'active' : '' ;
             }
         ?>
-        <nav>
-             <a class=" {{ activateMenu('/') }}" 
-                href="{{ route('home') }}">
-                Inicio
-            </a>
-            <a class=" {{ activateMenu('saludos/*') }}"  
-                href="{{  route('saludos', 'YOMerengues') }}">
-                Saludo
-            </a> <!-- MAndando parametro opcional -->
-            <a class=" {{ activateMenu('mensajes/create') }}"  
-                href="{{  route('mensajes.create') }}">
-                Contacto
-           
+        
+        
+        <nav class="navbar navbar-default" role="navigation">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="#">Title</a>
+                </div>
             
-            @if(auth()->check())
-                <a class=" {{ activateMenu('mensajes') }}"  
-                    href="{{  route('mensajes.index') }}">
-                    Mensajes
-                </a>
-                <a href="/logout">Cerrar sesion de {{auth()->user()->name}}</a>
-            @endif
-
-            @if(auth()->guest())
-                <a class=" {{ activateMenu('login') }}"  
-                    href="{{route('login')}}">
-                    login
-                </a>
-            @endif
-            
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class="collapse navbar-collapse navbar-ex1-collapse">
+                    <ul class="nav navbar-nav">
+                        <li class=" {{ activateMenu('/') }}">
+                            <a 
+                                href="{{ route('home') }}">
+                                Inicio
+                            </a>
+                        </li>
+                        <li class=" {{ activateMenu('saludos/*') }}">
+                            <a 
+                                href="{{  route('saludos', 'YOMerengues') }}">
+                                Saludo
+                            </a> <!-- MAndando parametro opcional -->
+                        </li>
+                        <li class=" {{ activateMenu('mensajes/create') }}" >
+                            <a 
+                                href="{{  route('mensajes.create') }}">
+                                Contacto
+                            </a>
+                        </li>
+                    
+                        
+                        @if(auth()->check())
+                            <li class=" {{ activateMenu('mensajes*') }}">
+                                <a   
+                                    href="{{  route('mensajes.index') }}">
+                                    Mensajes
+                                </a>
+                            </li>
+                            
+                        @endif
+                        
+                    </ul>
+                    
+                    <ul class="nav navbar-nav navbar-right">
+                        @if(auth()->guest())
+                            <li class=" {{ activateMenu('login') }}">
+                                <a   
+                                    href="{{route('login')}}">
+                                    login
+                                </a>
+                            </li>
+                        @else
+                            <li>
+                                <a href="/logout">Cerrar sesion de {{auth()->user()->name}}</a>
+                            </li>
+                        @endif
+                        
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="#">Action</a></li>
+                                <li><a href="#">Another action</a></li>
+                                <li><a href="#">Something else here</a></li>
+                                <li><a href="#">Separated link</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div><!-- /.navbar-collapse -->
+            </div>
         </nav>
     </header>
+    
+    <div class="container">
+        @yield('content') <!-- section para que vaya el contenido de las otras plantillas-->
 
-    @yield('content') <!-- section para que vaya el contenido de las otras plantillas-->
-
-    <footer>
-        Copiright {{date('Y')}}
-    </footer>
+        <footer>
+            Copiright {{date('Y')}}
+        </footer>
+    </div>
+    
 </body>
 </html>
